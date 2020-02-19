abba4c30f401de326d9246f44ed1dcc236cd08fa
Laurence Berube Cote <lberubecote@hotmail.com>
2020-02-19 16:32:05 -0500

    yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'Epicure', category: 'french', address: '123 fake street')
+Restaurant.create(name: 'Petit Italien', category: 'italian', address: '234 Laurier street')
+Restaurant.create(name: 'Maison VIP', category: 'chinese', address: '544 St-Urbain street')
+Restaurant.create(name: 'Di Stagio', category: 'italian', address: '677 St-Denis street')
+Restaurant.create(name: 'Chez Levesque', category: 'french', address: '6983 Bernard street')
